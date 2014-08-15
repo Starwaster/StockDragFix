40981a8e38cfb00cd3847dd254358655f9d3658e
Chase <chasesan@gmail.com>
2014-08-15 00:34:25 -0400

    Add stack drag multiplier logic.
    Let's nose cones do their work.

--- StockDragFix/SDFPartModule.cs
+++ StockDragFix/SDFPartModule.cs
@@ -12,6 +12,8 @@ namespace StockDragFix {
 		[KSPField()]
 		float minimum_drag = -1f;
 
+		float drag_multiplier = 1f;
+
 		public override void OnStart(PartModule.StartState start) {
 			base.OnStart(start);
 			print("StockDragFix PartModule: OnStart()");
@@ -28,13 +30,29 @@ namespace StockDragFix {
 				}
 				print(part.name + " drag set to " + minimum_drag.ToString() + " / " + maximum_drag.ToString());
 			}
+
+			//check if we have stack attach nodes
+			foreach(AttachNode node in part.attachNodes) {
+				if(node.nodeType == AttachNode.NodeType.Stack) {
+					if(node.attachedPart == null) {
+						//add a 5% drag increase per unfilled stack attach point
+						//since cones only have 1 stack node, and it's always filled... :D
+						//this means that minimally we will have 5% extra drag for each stack due to the engines
+						drag_multiplier += 0.05f;
+					}
+				}
+			}
 		}
 
 		public void FixedUpdate() {
 			part.minimum_drag = maximum_drag * part.mass / (part.mass + part.GetResourceMass());
 			part.maximum_drag = maximum_drag * part.mass / (part.mass + part.GetResourceMass());
+
 			part.minimum_drag *= Mathf.Abs(StockDragFix.dragScale);
 			part.maximum_drag *= Mathf.Abs(StockDragFix.dragScale);
+
+			part.minimum_drag *= drag_multiplier;
+			part.maximum_drag *= drag_multiplier;
 		}
 	}
 }
